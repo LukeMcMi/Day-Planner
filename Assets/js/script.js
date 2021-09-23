b2003c9dc65574c77bac092bc4e153a8b21ad916
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-09-23 17:39:04 +1000

    Added conditional formatting based on time in java and added save buttons and functionality.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,66 +11,66 @@ console.log(currentTime);
 var hrPeriods = [
     {
         id: "0",
-        hr: "09",
+        hr: "9",
         time: "09",
-        ampm: "am",
-        note: ""
+        ampm: " am",
+        notes: ""
     },
     {
         id: "1",
         hr: "10",
         time: "10",
-        ampm: "am",
-        note: ""
+        ampm: " am",
+        notes: ""
     },
     {
         id: "2",
         hr: "11",
         time: "11",
-        ampm: "am",
-        note: ""
+        ampm: " am",
+        notes: ""
     },
     {
         id: "3",
         hr: "12",
         time: "12",
-        ampm: "pm",
-        note: ""
+        ampm: " pm",
+        notes: ""
     },
     {
         id: "4",
-        hr: "01",
+        hr: "1",
         time: "13",
-        ampm: "pm",
-        note: ""
+        ampm: " pm",
+        notes: ""
     },
     {
         id: "5",
-        hr: "02",
+        hr: "2",
         time: "14",
-        ampm: "pm",
-        note: ""
+        ampm: " pm",
+        notes: ""
     },
     {
         id: "5",
-        hr: "03",
+        hr: "3",
         time: "15",
-        ampm: "pm",
-        note: ""
+        ampm: " pm",
+        notes: ""
     },
     {
         id: "6",
-        hr: "04",
+        hr: "4",
         time: "16",
-        ampm: "pm",
-        note: ""
+        ampm: " pm",
+        notes: ""
     },
     {
         id: "7",
-        hr: "05",
+        hr: "5",
         time: "17",
-        ampm: "pm",
-        note: ""
+        ampm: " pm",
+        notes: ""
     }
 ]
 
@@ -82,7 +82,7 @@ function saveCalItems() {
 // show saved data
 function showCalItems() {
     hrPeriods.forEach(function(_thisHour) {
-        $('#${_thishr.id}').val(_thisHour.note);
+        $(`#${_thisHour.id}`).val(_thisHour.notes);
     })
 }
 
@@ -108,18 +108,18 @@ hrPeriods.forEach(function(thisHour) {
 
     // time fields
     var hrFields = $("<div>")
-    .text('${thishr.hr}${thishr.ampm}')
+    .text(`${thisHour.hr}${thisHour.ampm}`)
     .attr({
         "class": "col-md-2 hour"
     });
 
     // create cal items
-    var hourItem = $("<div>")
+    var hrPlan = $("<div>")
     .attr({
         "class": "col-md-9 description p-0"
     });
     var planData = $("<textarea>");
-    hrFields.append(planData);
+    hrPlan.append(planData);
     planData.attr("id", thisHour.id)
     if (thisHour.time < currentTime) {
         planData.attr ({
@@ -135,4 +135,25 @@ hrPeriods.forEach(function(thisHour) {
         })
     }
 
+    var saveButton = $("<i class='far fa-save fa-lg'></i>")
+    var savePlan = $("<button>")
+    .attr({
+        "class": "col-md-1 saveBtn"
+    });
+    savePlan.append(saveButton);
+    hrRow.append(hrFields, hrPlan, savePlan);
+
+})
+
+init();
+
+// save data for local storage
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+    var saveIndex = $(this).siblings(".description").children(".future").attr("id");
+    hrPeriods[saveIndex].notes = $(this).siblings(".description").children(".future").val();
+    console.log(saveIndex);
+
+    saveCalItems();
+    showCalItems();
 })
